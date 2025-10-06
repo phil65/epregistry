788b6bfc06a66547ab6c49024bfdf54b93852405
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-10-06 18:05:17 +0200

    chore: bump template

--- src/epregistry/__init__.py
+++ src/epregistry/__init__.py
@@ -1,8 +1,17 @@
+"""epregistry: A registry for entry points (cached and generically typed)"""
+
 from __future__ import annotations
 
 from importlib.metadata import version
 
 __version__ = version("epregistry")
+__title__ = "epregistry"
+__description__ = "A registry for entry points (cached and generically typed)"
+__author__ = "Philipp Temminghoff"
+__author_email__ = "philipptemminghoff@googlemail.com"
+__copyright__ = "Copyright (c) 2024 Philipp Temminghoff"
+__license__ = "MIT"
+__url__ = "https://github.com/phil65/epregistry"
 
 from importlib.metadata import EntryPoint
 from epregistry.epregistry import (
